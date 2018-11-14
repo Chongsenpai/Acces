7919f37fd264c617e3ead5878c8681d629155d75
Chongsenpai <37104800+Chongsenpai@users.noreply.github.com>
2018-11-14 11:07:44 +0100

    Update NameGenerator.py

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,1 +1,21 @@
+import random
+
+def importList(file):
+    f = open(file)
+    output = f.readlines()
+    f.close()
+    for i in range(len(output)):
+        output[i] = output[i].strip()
+    return output
+
+adj = importList("Adjektiver.txt")
+sub = importList("Substantiver.txt")
+
+def getRandom(list):
+    rand = random.choice(list)
+    return rand
+
+def makeAttack():
+    atk = str(getRandom(adj)) + " " + str(getRandom(sub)) + "!"
+    return atk
 
